6c0bc3a6b532b63e2c95d8877a78ecc94fb22420
JohnzExor <jimenojohnzyll@gmail.com>
2023-11-15 19:07:36 +0800

    added background image and blur effect

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,26 @@ import About from "./pages/About";
 import Header from "./components/Header";
 import { ThemeProvider } from "./components/theme/Theme-Provider";
 
+import background from "@/assets/background.svg";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {/* img src={background} className=" fixed object-cover h-screen w-full" /> */}
-      <Header />
-      <LandingPage />
-      <About />
-      <Projects />
-      <Contact />
+      <img
+        src={background}
+        alt="background"
+        className="fixed w-full object-cover h-screen md:h-min opacity-30"
+      />
+
+      <div className=" backdrop-blur-3xl">
+        <Header />
+        <LandingPage />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className=" p-4 w-full  flex justify-center gap-4">
+      <label className="text-xl flex items-center gap-1">
+        <p className=" font-semibold">
+          Johnz<span className=" font-thin"> 2023</span>
+        </p>
+      </label>
+    </footer>
+  );
+};
+
+export default Footer;
